d0d12aa0ea149131f3560c8ba3248beee4be80ac
Anna Tiberg <anastasia-elisabeth.tiberg@elev.ga.ntig.se>
2023-09-04 09:40:42 +0200

    test

---
+++ src/Al.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Al
+{
+    public static void main(String[] args) {
+        Scanner tb = new Scanner(System.in) {
+            String name = "tb ";
+            String text = tb.nextLine();
+            System.out.println("Du skrev "+ text);
+        }
+
+    }
+}
